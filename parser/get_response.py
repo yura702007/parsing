9ea882a10d362063c14162557323d10e10c61fd3
yura702007 <82802568+yura702007@users.noreply.github.com>
2022-08-24 15:48:29 +0300

    response ==> resp

--- parser/get_response.py
+++ parser/get_response.py
@@ -4,7 +4,7 @@ from settings import URL, HEADERS
 
 def get_response(url=URL, headers=HEADERS):
     """
-    Получение ответа от главной страницы сайта
+    Получение ответа от страницы сайта
     """
     resp = requests.get(url=url, headers=headers, allow_redirects=True)
     return resp
